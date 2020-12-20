fd6c91f2b2ea4f99528d7452d32be5fec2ece6fa
Edd <eddsm1th@hotmail.com>
2020-12-20 17:44:58 +0000

    improved sphere generation

--- src/js-components/celestial_body_generator.js
+++ src/js-components/celestial_body_generator.js
@@ -3,36 +3,29 @@
 		layer_options = { ...get_celestial_body_defaults(), ...layer_options };
 		layer_options.final_frequency_count = get_final_frequency_count( layer_options );
 
-		// move to own file
+		// move sphere generation to own file
 		const 	flat_data = get_flat_array( layer_options ),
 				octahedron_data = map_flat_data_to_octahedron( flat_data, layer_options ),
-				hemispherical_data = map_octahedron_data_to_hemisphere( octahedron_data.map( i => i ), layer_options );
-
-		let 	last_index = hemispherical_data.flat()[ hemispherical_data.flat().length - 1 ].index + 1;
-
-		const 	spherical_data = [ ...hemispherical_data, ...hemispherical_data.reverse().slice( 1, hemispherical_data.length ).map( layer => layer.map( ( layer_item ) => {
-					return { ...layer_item, ...{ y: layer_item.y * -1, index: ( last_index ++ ) } }
-				} ) ) ];
-
-		const array_to_render = spherical_data;
+				hemispherical_data = map_octahedron_data_to_hemisphere( octahedron_data.map( i => i ), layer_options ),
+				spherical_data = [ ...hemispherical_data, ...hemispherical_data.reverse().slice( 1, hemispherical_data.length ).map( layer => layer.map( ( layer_item ) => ( { ...layer_item, ...{ y: layer_item.y * -1 } } ) ) ) ];
 
 		const 	geometry = new THREE.Geometry(),
 				scene = new THREE.Scene(),
 				camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, .1, 1000 ),
 				renderer = new THREE.WebGLRenderer();
 
+		camera.position.z = 1000; // make this more relative to planet size and window
 		renderer.setSize( window.innerWidth, window.innerHeight );
 		document.body.appendChild( renderer.domElement );
 
-		camera.position.z = 1200; // make this more relative to planet size and window
+		spherical_data.flat().forEach( ( item, index ) => {
+			item.index = index;
+			geometry.vertices.push( new THREE.Vector3( item.x, item.y, item.z ) )
+		} );
 
-		array_to_render.flat().forEach( ( item ) => geometry.vertices.push( new THREE.Vector3( item.x, item.y, item.z ) ) );
-
-		const layers_to_loop_through = Math.ceil( array_to_render.length / 2 );
-
-		for ( let i = 1; i < layers_to_loop_through; i ++ ) {
-			const 	current_layer = array_to_render[ i ],
-					polar_current_layer = array_to_render[ array_to_render.length - 1 - i ],
+		for ( let i = 1; i < Math.ceil( spherical_data.length / 2 ); i ++ ) {
+			const 	current_layer = spherical_data[ i ],
+					polar_current_layer = spherical_data[ spherical_data.length - 1 - i ],
 					points_per_side = ( current_layer.length / 4 );
 
 			let point_layer_offsets = 1;
@@ -41,52 +34,46 @@
 				if ( index % points_per_side == 0 ) {
 					point_layer_offsets -= 1;
 
-					// top down
-					geometry.faces.push( new THREE.Face3(
-						current_layer[ index ].index,
-						array_to_render[ i - 1 ][ index + point_layer_offsets ].index,
-						( current_layer[ index + 1 ] || current_layer[ 0 ] ).index
-					) );
+					const create_corner_panel = ( sent_arr, sent_index ) => {
+						geometry.faces.push( new THREE.Face3(
+							sent_arr[ index ].index,
+							spherical_data[ sent_index ][ index + point_layer_offsets ].index,
+							( sent_arr[ index + 1 ] || sent_arr[ 0 ] ).index
+						) );
+					}
 
-					// bottom up
-					geometry.faces.push( new THREE.Face3(
-						polar_current_layer[ index ].index,
-						array_to_render[ array_to_render.length - i ][ index + point_layer_offsets ].index,
-						( polar_current_layer[ index + 1 ] || polar_current_layer[ 0 ] ).index
-					) );
+					create_corner_panel( current_layer, ( i - 1 ) );
+					create_corner_panel( polar_current_layer, ( spherical_data.length - i  ) );
 				} else {
-					geometry.faces.push( new THREE.Face3(
-						current_layer[ index ].index,
-						array_to_render[ i - 1 ][ index + point_layer_offsets - 1 ].index,
-						( array_to_render[ i - 1 ][ index + point_layer_offsets ] || array_to_render[ i - 1 ][ 0 ] ).index,
-					) );
-					geometry.faces.push( new THREE.Face3(
-						current_layer[ index ].index,
-						( array_to_render[ i - 1 ][ index + point_layer_offsets ] || array_to_render[ i - 1 ][ 0 ] ).index,
-						( current_layer[ index + 1 ] || current_layer[ 0 ] ).index
-					) );
+					const create_upper_panel = ( sent_arr, sent_index ) => {
+						geometry.faces.push( new THREE.Face3(
+							sent_arr[ index ].index,
+							spherical_data[ sent_index ][ index + point_layer_offsets - 1 ].index,
+							( spherical_data[ sent_index ][ index + point_layer_offsets ] || spherical_data[ sent_index ][ 0 ] ).index,
+						) );
+					},
+					create_lower_panel = ( sent_arr, sent_index ) => {
+						geometry.faces.push( new THREE.Face3(
+							sent_arr[ index ].index,
+							( spherical_data[ sent_index ][ index + point_layer_offsets ] || spherical_data[ sent_index ][ 0 ] ).index,
+							( sent_arr[ index + 1 ] || sent_arr[ 0 ] ).index
+						) );
+					};
 
-
-					geometry.faces.push( new THREE.Face3(
-						polar_current_layer[ index ].index,
-						array_to_render[ array_to_render.length - i ][ index + point_layer_offsets - 1 ].index,
-						( array_to_render[ array_to_render.length - i ][ index + point_layer_offsets ] || array_to_render[ array_to_render.length - i ][ 0 ] ).index,
-					) );
-					geometry.faces.push( new THREE.Face3(
-						polar_current_layer[ index ].index,
-						( array_to_render[ array_to_render.length - i ][ index + point_layer_offsets ] || array_to_render[ array_to_render.length - i ][ 0 ] ).index,
-						( polar_current_layer[ index + 1 ] || polar_current_layer[ 0 ] ).index
-					) );
+					create_upper_panel( current_layer, ( i - 1 ) );
+					create_upper_panel( polar_current_layer, ( spherical_data.length - i ) );
+					create_lower_panel( current_layer, ( i - 1 ) );
+					create_lower_panel( polar_current_layer, ( spherical_data.length - i ) );
 				}
 			} );
 		}
 
-		const material = new THREE.MeshBasicMaterial( {
-			color: 0x00ff00,
-			wireframe: true,
-			side: THREE.DoubleSide,
-		} );
-		const terrain = new THREE.Mesh( geometry, material );
+		const 	material = new THREE.MeshBasicMaterial( {
+					color: 0x00ff00,
+					wireframe: true,
+					side: THREE.DoubleSide,
+				} ),
+				terrain = new THREE.Mesh( geometry, material );
 
 		scene.add( terrain );
 
@@ -95,12 +82,9 @@
 		function animate() {
 				requestAnimationFrame( animate );
 				renderer.render( scene, camera );
-		}
-		animate();
+		} animate();
 
-		console.log( array_to_render );
-
-		// return spherical_data;
+		return spherical_data;
 	}
 
 	const apply_drag_controls = ( scene ) => {
@@ -135,15 +119,13 @@
 	}
 
 	const get_celestial_body_defaults = () => ( {
-		'base_frequency' : 8,
-		'layers' : 1,
+		'base_frequency' : 12,
+		'layers' : 4,
 		'radius' : 600,
 		'amp_bias' : 0,
 	} )
 
 	const map_octahedron_data_to_hemisphere = ( octahedron_data, { radius } ) => {
-		// return octahedron_data;
-
 		const octahedron_data_to_calculate = [ ...octahedron_data ].slice( 1 ).map( layer => layer.slice( 0, layer.length / 4 ) );
 
 		octahedron_data_to_calculate.forEach( ( layer, index ) => {
@@ -174,48 +156,27 @@
 
 	const map_flat_data_to_octahedron = ( flat_array, { radius, final_frequency_count } ) => {
 		const layer_angle_increment = ( 90 / ( final_frequency_count - 1 ) );
-
-		let cumulative_traversial_distance,
-			current_index = 0;
 			
 		return flat_array.map( ( layer, index ) => { 	
 			const 	points_per_side = Math.ceil( ( layer.length / 4 ) ),
 					angle_increment = 90 / points_per_side,
-					layer_traversial_radians = degree_in_radians( -45 + ( layer_angle_increment * index ) );
-
-			cumulative_traversial_distance = ( ( Math.tan( layer_traversial_radians ) * radius ) + radius ) / 2;
-					
-			const 	stepped_side_array = new Array( points_per_side ).fill( null ).map( ( item, stepped_side_index ) => {
-						const radians = degree_in_radians( -45 + ( angle_increment * stepped_side_index ) );	
-
-						return Math.tan( radians ) * cumulative_traversial_distance;
-					} ),
-					reverse_stepped_side_array = stepped_side_array.map( stepped_side_val => stepped_side_val * -1 );
-
-			const 	start_value = new Array( points_per_side ).fill( cumulative_traversial_distance * -1 ),
-					end_value = start_value.map( end_val => end_val *= -1 );
-
-			const 	x_layer_data = [ stepped_side_array, end_value, reverse_stepped_side_array, start_value ].flat(),
+					layer_traversial_radians = degree_in_radians( -45 + ( layer_angle_increment * index ) ),
+					cumulative_traversial_distance = ( ( Math.tan( layer_traversial_radians ) * radius ) + radius ) / 2,
+					stepped_side_array = new Array( points_per_side ).fill( null ).map( ( item, stepped_side_index ) => Math.tan( degree_in_radians( -45 + ( angle_increment * stepped_side_index ) ) ) * cumulative_traversial_distance ),
+					reverse_stepped_side_array = stepped_side_array.map( stepped_side_val => stepped_side_val * -1 ),
+					start_value = new Array( points_per_side ).fill( cumulative_traversial_distance * -1 ),
+					end_value = start_value.map( end_val => end_val *= -1 ),
+					x_layer_data = [ stepped_side_array, end_value, reverse_stepped_side_array, start_value ].flat(),
 					z_layer_data = [ end_value, reverse_stepped_side_array, start_value, stepped_side_array ].flat();
 
 			return layer.map( ( layer_item, sub_index ) => ( {
 				'x' : x_layer_data[ sub_index ],
 				'y' : cumulative_traversial_distance * -1 + radius,
 				'z' : z_layer_data[ sub_index ],
-				'index' : current_index ++, 
 			} ) );
 		} );
 	}
 
 	const get_flat_array = ( { final_frequency_count } ) => [ [ {} ], ...new Array( final_frequency_count - 1 ).fill().map( ( layer, index ) => new Array( 4 * ( index + 1 ) ).fill( {} ) ) ];
 
-	const get_final_frequency_count = ( { base_frequency, layers } ) => {
-		let count = base_frequency;
-
-		for ( let i = 1; i < layers; i ++ ) {
-			count *= 2;
-			count -= 1;
-		}
-
-		return count;
-	}
+	const get_final_frequency_count = ( { base_frequency, layers } ) => layers == 1 ? base_frequency : ( base_frequency * ( Math.pow( 2, layers ) ) / 2 - ( new Array( layers ).fill().map( ( i, index ) => Math.floor( Math.pow( 2, ( index - 1 ) ) ) ).reduce( ( a, b ) => a + b ) ) );
